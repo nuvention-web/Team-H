097aa0d7237893c47f9cab529540a4fbeb6a8405
jordanc44 <clark.jordan@outlook.com>
2018-01-15 16:49:07 -0600

    Add files via upload

--- src/app/git-id-info.service.ts
+++ src/app/git-id-info.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GitIdInfo } from './github-id';
 import { Observable } from 'rxjs/Observable';
+import {RepoInfo} from './repo';
 
 const githubAPI = 'https://api.github.com/';
 
@@ -14,4 +15,10 @@ export class GitIdInfoService {
     const userAPI = githubAPI + 'users/';
     return(this.http.get<GitIdInfo>(userAPI + login));
   }
+
+  GetRepoInfo(login: string): Observable<RepoInfo> {
+    const userAPI = githubAPI + 'repos/';
+    return(this.http.get<RepoInfo>(userAPI + login));
+  }
+ 
 }
---
+++ src/app/issues.ts
@@ -0,0 +1,15 @@
+import { RepoInfo } from './repo';
+
+export interface IssuesInfo {
+    login: string;
+    name?: string;
+    bio?: string;
+    issues_url?: string;
+    location?: string;
+    company?: string;
+    html_url?: string;
+    created_at?: string;
+    public_repos?: string;
+    favorite?: boolean;
+}
+
