0cd51317f4d4aab74d969fb6ad0d16096be89b8d
jordanc44 <clark.jordan@outlook.com>
2018-01-21 16:33:56 -0600

    Delete repo.ts

--- repo.ts
+++
@@ -1,9 +0,0 @@
-export interface RepoInfo {
-    name: string;
-    login?: string;
-    description?: string;
-    issues_url?: string;
-    milestones_url?: string;
-    full_name?: string;
-    
-}
