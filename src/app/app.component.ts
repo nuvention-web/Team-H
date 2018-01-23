a1764adb91ebde732866332fdb929bbb630bc815
Yousef Issa <yousef@u.northwestern.edu>
2018-01-23 00:39:12 -0600

    fixed bug where selecting repo does not actually change the repo variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,84 +27,93 @@ export class AppComponent {
   private getRepolistsub: Subscription;
   private getIssuesub: Subscription;
   private getMstonesub: Subscription;
-  
+
   errorMessage = null;
 
-  constructor(private ids: GitIdInfoService) { }
+  constructor(private ids: GitIdInfoService) {}
 
   addGhId(toadd: string) {
     this.errorMessage = null;
-    this.getGitsub = this.ids.GetGitIdInfo(toadd).subscribe( info => {
-      this.ghIds.push(info);
+    this.getGitsub = this.ids.GetGitIdInfo(toadd).subscribe(info => {
+        this.ghIds.push(info);
       },
       error => {
         console.log('error:', error);
         this.errorMessage = error.message;
       });
-      this.ghId = '';
+    this.ghId = '';
+  }
+
+
+  getRepo(toadd: string, getIssuesFunct: boolean) {
+    this.class = 'none';
+    this.errorMessage = null;
+    // repo field takes repo link
+    let cleanedLink = toadd.split('/');
+    let repoTag = cleanedLink.slice(cleanedLink.length - 2, cleanedLink.length).join('/');
+    // checks if name was passed in by another function
+    if (getIssuesFunct) {
+      repoTag = toadd;
     }
+    this.getReposub = this.ids.GetRepoInfo(repoTag).subscribe(info => {
+        this.repos.push(info['name'])
+        this.repos.push(info['login'])
+        this.repos.push(info['issues_url'])
+        this.repos.push(info['milestones_url'])
+        console.log(this.repos);
+        this.getIssues();
+        // this.getMilestones();
 
+      },
+      error => {
+        console.log('error:', error);
+        this.errorMessage = error.message;
+      });
 
-      getRepo(toadd: string) {
-        this.class = 'none';
-        this.errorMessage = null;
-        this.getReposub = this.ids.GetRepoInfo(toadd).subscribe( info => {
-          this.repos.push(info['name'])
-          this.repos.push(info['login'])
-          this.repos.push(info['issues_url'])
-          this.repos.push(info['milestones_url'])
-          console.log(this.repos);
-          this.getIssues();
-         // this.getMilestones();
-          
-          },
-          error => {
-            console.log('error:', error);
-            this.errorMessage = error.message;
-          });
+    this.repos = [];
+    this.issues = [];
+    console.log(this.repos);
+  }
 
-          
-          
+  getRepoList(toadd: string) {
+
+    this.getRepolistsub = this.ids.GetRepoList(toadd).subscribe(info => {
+      info.forEach(element => {
+        this.repolist.push(element as RepoInfo);
+
+      });
+      console.log(this.repolist);
+    })
+
+  }
+
+  getIssues() {
+    console.log(this.repos)
+    this.getIssuesub = this.ids.GetIssue(this.repos[2].substr(0, (this.repos[2].length - 9))).subscribe(info => {
+      info.forEach(element => {
+        this.issues.push(element as IssuesInfo);
+      });
+      console.log(this.issues);
+      if (this.issues.length == 0) {
+        this.errorMessage = 'There are no issues for this repository.';
       }
 
-      getRepoList(toadd: string) {
-       
-        this.getRepolistsub = this.ids.GetRepoList(toadd).subscribe( info => {
-          info.forEach(element => {
-          this.repolist.push(element as RepoInfo);
-          
-        });
+    })
 
-        console.log(this.repolist); 
-        
-       
-      
-      })
-    }
-
-      getIssues() {
-      
-        this.getIssuesub = this.ids.GetIssue(this.repos[2].substr(0, (this.repos[2].length - 9))).subscribe( info => {
-          info.forEach(element => {
-            this.issues.push(element as IssuesInfo);
-          });
-          console.log(this.issues);
-        
-})
-}
+  }
 
 
-     repoName() {
-        return this.repos[0];
-      }
-    
-      repoOwner() {
-        return this.repos[1];
-      }
+  repoName() {
+    return this.repos[0];
+  }
 
-   
+  repoOwner() {
+    return this.repos[1];
+  }
 
- /* getMilestones() {
+
+
+  /* getMilestones() {
       
   this.getMstonesub = this.ids.GetIssue(this.repos[3].substr(0, (this.repos[3].length - 9))).subscribe( info => {
     info.forEach(element => {
@@ -128,6 +137,3 @@ printMilestones()
   }
   */
 }
-
-
-
